d688026e932abd552a2beb21e4f8df5e2a493abe
yangpengli_glb <yangpengli213gmail@lyp.com>
2019-01-17 10:30:38 +0800

    计算错误修改

--- src/main/java/com/lyp/mr/worldcount/WordCountReduce.java
+++ src/main/java/com/lyp/mr/worldcount/WordCountReduce.java
@@ -18,23 +18,28 @@ import org.apache.hadoop.mapreduce.Reducer;
  * @version 1.0
  *
  */
-public class WordCountReduce extends Reducer<Text ,IntWritable,Text,IntWritable>
-{
-	IntWritable  v = new IntWritable();
-	int sum = 0;
+public class WordCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+	IntWritable v = new IntWritable();
+	int sum;
+
+	/**
+	 * This method is called once for each key. Most applications will define their
+	 * reduce class by overriding this method. The default implementation is an
+	 * identity function.
+	 */
 	@Override
-	protected void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException 
-	{
+	protected void reduce(Text key, Iterable<IntWritable> values, Context context)
+			throws IOException, InterruptedException {
+		// 1.累加求和
+		sum=0;
+		for (IntWritable value : values) {
+			
+			sum += value.get();
+		}
 		
-		//1.累加求和
-		for (IntWritable value : values) 
-		{
-			 
-			 sum +=value.get();
-		} 
-		//2.写出 lyp2
+		// 2.写出 lyp2
 		v.set(sum);
-		
+		System.out.println("----sum------"+sum);
 		context.write(key, v);
 	}
 	
